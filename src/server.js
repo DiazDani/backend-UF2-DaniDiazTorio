f859805eb75d80b22e8a331b4cd74764f57940ad
diazdani <daniel.diaz@institutvidreres.cat>
2023-06-02 13:28:35 +0200

    commit 1 i 2

--- src/server.js
+++ src/server.js
@@ -26,5 +26,39 @@ connection.connect((error) => {
     console.error('Error al conectar a la base de dades:', error);
   } else {
     console.log('Conexió exitosa');
+    afegirCampAlumVirgen();
   }
+});
+
+function afegirCampAlumVirgen() {
+  const query = `
+    ALTER TABLE ALUMNES
+    ADD ALUM_VIRGEN INTEGER DEFAULT 0;
+  `;
+
+  connection.query(query, (error) => {
+    if (error) {
+      console.error('Error:', error);
+      throw new Error('Maria, maria...');
+    } else {
+      console.log('Campo ALUM_VIRGEN afegit exitosament');
+    }
+  });
+}
+
+app.get('/llistaProf', (req, res) => {
+  const query = `
+    SELECT PROF_DNI, PROF_NOM, PROF_COGNOM_1, PROF_COGNOM_2, PROF_TELEFON
+    FROM PROFESSOR, DEPARTAMENT
+    WHERE PROF_DEPT_CODI = DEPT_CODI AND PROF_CATEGORIA = 'Associat' AND DEPT_NOM != 'MULTIMEDIA';
+  `;
+
+  connection.query(query, (error, results) => {
+    if (error) {
+      console.error('Error:', error);
+      res.status(500).json({ error: 'Error al obtenir les dades' });
+    } else {
+      res.json(results);
+    }
+  });
 });
